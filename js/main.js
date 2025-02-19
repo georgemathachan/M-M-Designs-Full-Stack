599f1ec03560e76c4de8d40674afa103ea789532
georgemathachan <gmathachan44@gmail.com>
2025-02-19 23:33:04 +0000

    homepage nearly done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 //general scipts for page interactions (loading animations, global event listeners)
+
+// Get the navbar element
+const navbar = document.querySelector('.navbar');
+
+// Listen for the scroll event
+window.addEventListener('scroll', () => {
+  // If the page is scrolled down, add the 'scrolled' class
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    // If at the top of the page, remove the 'scrolled' class
+    navbar.classList.remove('scrolled');
+  }
+});
