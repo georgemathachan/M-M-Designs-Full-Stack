5ac520a569a35a7026cc2b3d5f5220517990adb2
georgemathachan <gmathachan44@gmail.com>
2025-02-23 16:01:13 +0000

    product detail page

--- js/product.js
+++ js/product.js
@@ -1,1 +1,9 @@
 //handling product page interactions (image zoom, variations)
+
+//for the description on product detail page
+document.querySelectorAll(".description-toggle").forEach(button => {
+    button.addEventListener("click", () => {
+        const item = button.parentElement;
+        item.classList.toggle("active");
+    });
+});
